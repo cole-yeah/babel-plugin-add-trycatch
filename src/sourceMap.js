e7595c0c697995cc6d02c1b462e6e1c056e7b226
chenlc <243461254@qq.com>
2023-06-06 23:09:14 +0800

    sourcemap

---
+++ src/sourceMap.js
@@ -0,0 +1,18 @@
+var map = new SourceMapGenerator({
+  file: "source-mapped.js"
+});
+
+map.addMapping({
+  generated: {
+    line: 10,
+    column: 35
+  },
+  source: "foo.js",
+  original: {
+    line: 33,
+    column: 2
+  },
+  name: "christopher"
+});
+
+console.log(map.toString());
